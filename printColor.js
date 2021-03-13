8ebd2ed083c941c0a31de0b25136f3cf97465728
SaraB <baas.burke@gmail.com>
2021-03-13 04:52:46 -0500

    changed body bg color

--- printColor.js
+++ printColor.js
@@ -5,7 +5,7 @@ var display = color.randomColor();
 //create a server object:
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<h1 style="color:#000; text-align:center; font-size:3em; text-shadow:1px 1px 5px #124; border: 5px solid'+ headBorder +';">Ten different color sections!</h1>'); //write a response to the client
+  res.write('<h1 style="color:#000; text-align:center; font-size:3em; text-shadow:1px 1px 3px #262626; box-shadow:2px 2px 2px #000; border: 5px solid'+ headBorder +';">Ten different color sections!</h1>'); //write a response to the client
   res.write('<p style="background:'+ color1 +'; color:#fff; width:50%; border: 2px solid #000; text-align:center; text-shadow:2px 2px 2px #000; font-size:2em;  height:69px; margin: auto; border-radius:5px;">'+color1+'</p>');
   res.write('<p style="background:'+ color2 +'; color:#fff; width:50%; border: 2px solid #000; text-align:center; text-shadow:2px 2px 2px #000; font-size:2em; height:69px; margin: auto; border-radius:5px;">'+color2+'</p>');
   res.write('<p style="background:'+ color3 +'; color:#fff; width:50%; border: 2px solid #000; text-align:center; text-shadow:2px 2px 2px #000; font-size:2em; height:69px; margin: auto; border-radius:5px;">'+color3+'</p>');
@@ -16,6 +16,7 @@ http.createServer(function (req, res) {
   res.write('<p style="background:'+ color8 +'; color:#fff; width:50%; border: 2px solid #000; text-align:center; text-shadow:2px 2px 2px #000; font-size:2em; height:69px; margin: auto; border-radius:5px;">'+color8+'</p>');
   res.write('<p style="background:'+ color9 +'; color:#fff; width:50%; border: 2px solid #000; text-align:center; text-shadow:2px 2px 2px #000; font-size:2em; height:69px; margin: auto; border-radius:5px;">'+color9+'</p>');
   res.write('<p style="background:'+ color10 +'; color:#fff; width:50%; border: 2px solid #000; text-align:center; text-shadow:2px 2px 2px #000; font-size:2em; height:69px; margin: auto; border-radius:5px;">'+color10+'</p>');
+  res.write('<body style="background: #e0ebeb;"></body>')
   
   res.end(); //end the response
 }).listen(4000);
