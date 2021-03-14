5a7ebe9d2e82a688ff8bb9030877ec32c02d27d6
SaraB <baas.burke@gmail.com>
2021-03-13 21:00:46 -0500

    created 25States folder

---
+++ 25States/sortStates.js
@@ -0,0 +1,33 @@
+exports.states = function(){
+
+state = [
+"Massachusetts",
+"Alaska",
+"Arizona",
+"New York", 
+"California",
+"Florida",
+"New Hampshire",
+"Ohio",
+"Connecticut",
+"Vermont",
+
+"Rhode Island",
+"Maryland",
+"Oregon",
+"Colorado",
+"Connecticut",
+"Michigan",
+"Pennsylvania",
+"Washington",
+"Utah",
+"Texas",
+
+"Nevada",
+"New Jersey",
+"Maine",
+"Mississippi",
+"Arkansas",
+]
+
+}
