7a748690b9486d53d739b438c5f9dfedddcc6d15
SaraB <baas.burke@gmail.com>
2021-03-14 23:01:39 -0400

    changed id name to match HTML id

---
+++ 25States/displayST.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 2000
+
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+    res.sendFile('/index.html',{ root: __dirname })
+})
+
+app.listen(port, () => console.log('The server running on Port '+ port));
---
+++ 25States/public/sortStates.js
@@ -0,0 +1,40 @@
+    const stateCapitals = [
+        {state:"Massachusetts", capital:"Boston"},
+        {state:"Alaska", capital:"Juneau"},
+        {state:"Arizona", capital:"Phoenix"},
+        {state:"New York", capital:"Albany"},
+        {state:"California", capital:"Sacramento"},
+        {state:"Florida", capital:"Tallahassee"},
+        {state:"New Hampshire", capital:"Concord"},
+        {state:"Ohio", capital:"Columbus"},
+        {state:"Connecticut", capital:"Hartford"},
+        {state:"Vermont", capital:"Montpelier"},
+    
+        {state:"Rhode Island", capital:"Providence"},
+        {state:"Maryland", capital:"Annapolis"},
+        {state:"Oregon", capital:"Salem"},
+        {state:"Colorado", capital:"Denver"},
+        {state:"Connecticut", capital:"Hartford"},
+        {state:"Michigan", capital:"Lansing"},
+        {state:"Pennsylvania", capital:"Harrisburg"},
+        {state:"Washington", capital:"Olympia"},
+        {state:"Utah", capital:"Salt Lake City"},
+        {state:"Texas", capital:"Austin"},
+    
+        {state:"Nevada", capital:"Carson City"},
+        {state:"New Jersey", capital:"Trenton"},
+        {state:"Maine", capital:"Augusta"},
+        {state:"Mississippi", capital:"Jackson"},
+        {state:"Arkansas", capital:"Little Rock"} 
+    ];
+    
+    //stateCapitals.forEach((value) => value.capital);
+const sortST = {};
+// sort in ascending (1) order by length
+const sort = { state: 1};
+const display = stateCapitals
+.find(sortST)
+.sort(sort)
+.map(st => `<tr><td>${st.state}</td><td>${st.capital}</td></tr>`);
+
+document.getElementById("display").innerHTML = display
